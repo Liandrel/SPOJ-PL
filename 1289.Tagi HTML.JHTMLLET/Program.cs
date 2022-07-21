8f1cf04001f5f1614484737ad9a0563649514afe
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-07-21 22:15:27 +0200

    Add files via upload

---
+++ 1289.Tagi HTML.JHTMLLET/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            string text = "";
+
+            do
+            {
+                bool temp = false;
+                string linia = "";
+                text = Console.ReadLine();
+
+                for(int i = 0; i < text.Length; i++)
+                {
+                    
+                    if (temp)
+                    {
+                        if (text[i] == '>')
+                        {
+                            temp = false;
+                        }
+                        else
+                        {
+                            linia+= text[i].ToString().ToUpper();
+                        }
+                    }
+                    if (text[i] == '<')
+                    {
+                        temp = true;
+                        linia += text[i];
+                    }
+                    else if(!temp)
+                    {
+                        linia += text[i];
+                    }
+                }
+                Console.WriteLine(linia);
+            }
+            while (text != null);
+        }
+        catch (Exception ex) { Environment.Exit(0); }
+
+    }
+}
