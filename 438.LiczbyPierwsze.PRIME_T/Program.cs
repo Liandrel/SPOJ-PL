366853539797af188fba58c7ae344356e84dede3
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-07-21 22:08:10 +0200

    Add files via upload

---
+++ 438.LiczbyPierwsze.PRIME_T/Program.cs
@@ -0,0 +1,34 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int ile = int.Parse(Console.ReadLine()); 
+        
+        for (int i = 0; i < ile; i++)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(liczbapierwsza(n));
+        }
+        
+    }
+    static string liczbapierwsza(int n)
+    {
+        if ((n == 2) || (n == 3))
+        {
+            return "TAK";
+        }
+        if ((n <= 1) || (n % 2 == 0) || (n % 3 == 0))
+        {
+            return "NIE";
+        }
+        for (int i = 5; i * i <= n; i += 6)
+        {
+            if (n % i == 0 || n % (i + 2) == 0)
+            {
+                return "NIE";
+            }
+        }
+        return "TAK";
+    }
+}
