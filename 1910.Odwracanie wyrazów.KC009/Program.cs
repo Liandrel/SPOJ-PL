78607de1b6c4f856cd8ad1e919bacd76d2e294fe
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-07-25 20:45:16 +0200

    Add files via upload

---
+++ 1910.Odwracanie wyrazów.KC009/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            string dane;
+            do
+            {
+                dane = Console.ReadLine();
+                char[] arr = dane.ToCharArray();
+                Array.Reverse(arr);
+                string wynik = new string(arr);
+                Console.WriteLine(arr);
+            }
+            while (dane != null);
+        }
+        catch (Exception ex)
+        {
+            Environment.Exit(0);
+        }
+
+    }
+}
