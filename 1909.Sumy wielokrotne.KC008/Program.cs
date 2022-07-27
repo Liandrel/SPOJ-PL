1a5559da30c04b32f1795dff6a515f524c51139b
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-07-27 21:48:33 +0200

    Add files via upload

---
+++ 1909.Sumy wielokrotne.KC008/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1909.Sumy_wielokrotne.KC008
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<long> wszystkie = new List<long>();
+            long sumawszystkich = 0;
+            try
+            {
+                string data = "";
+                do
+                {
+                    data = Console.ReadLine();
+                    long suma = 0;
+                    string[] datatable = data.Split(' ');
+
+                    foreach (string s in datatable)
+                        suma += long.Parse(s);
+                    Console.WriteLine(suma);
+                    wszystkie.Add(suma);
+                }
+                while (data != null);
+            }
+            catch (Exception ex)
+            {
+                foreach (var s in wszystkie)
+                    sumawszystkich += s;
+                Console.WriteLine(sumawszystkich);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ 1911.Zliczanie liczb i wyrazów.KC010/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _1911.Zliczanie_liczb_i_wyrazów.KC010
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string data;
+                do
+                {
+                    data = Console.ReadLine();
+                    string[] datatable = data.Split(' ');
+                    int liczby = 0, wyrazy = 0;
+                    for(int i = 0; i < datatable.Length; i++)
+                    {
+                        if ((Regex.Match(datatable[i], "^[0-9]*$").Success))
+                        {
+                            liczby++;
+                        }
+                        else
+                        {
+                            wyrazy++;
+                        }
+
+                    }
+                    Console.WriteLine($"{liczby} {wyrazy}");
+                }
+                while (data != "");
+            }
+            catch (Exception ex)
+            {
+                Environment.Exit(0);
+            }
+        }
+    }
+}
